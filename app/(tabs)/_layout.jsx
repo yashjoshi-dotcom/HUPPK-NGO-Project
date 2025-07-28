b014f522b493079b27924d2911727b231010e2e4
parth654 <parthba23@gmail.com>
2025-07-29 02:57:23 +0530

    fruits and vegetables classification first cut

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -44,6 +44,14 @@ export default function TabLayout() {
           tabBarIcon: ({ color, size }) => <FontAwesome name="home" size={size} color={color} />,
         }}
       />
+       <Tabs.Screen
+        name="classify"
+        options={{
+          title: 'Classification',
+          tabBarIcon: ({ color, size }) => <FontAwesome name="gamepad" size={size} color={color} />,
+        }}
+      />
+
       <Tabs.Screen
         name="explore"
         options={{
