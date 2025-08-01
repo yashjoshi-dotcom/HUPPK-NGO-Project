9b3d003a8beccf08e426e4ac751510c9535a3478
yashjoshi-dotcom <yashjoshimail@gmail.com>
2025-08-01 10:49:40 +0530

    update background

--- components/ui/Home/Header.jsx
+++ components/ui/Home/Header.jsx
@@ -4,7 +4,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from '../../../hooks/useThemeContext';
 
 // --- ASSET DEFINITIONS ---
-const landscapeBg = require('../../../assets/images/gradient-mountain-landscape_52683-77407.avif');
+const landscapeBg = require('../../../assets/images/gradient-mountain-landscape_52683-77407.png');
 const avatar = require('../../../assets/images/image-removebg-preview.png');
 // --------------------------
 
